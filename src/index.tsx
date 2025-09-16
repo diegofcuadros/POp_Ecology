25571cb3b1a031809b219746263c087a4552bd59
diegofcuadros <dfcuad2@gmail.com>
2025-09-16 16:58:41 -0400

    Add React project structure for deployment

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PopulationEcologyPlatform from './PopulationEcologyPlatform';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <PopulationEcologyPlatform />
+  </React.StrictMode>
+);
